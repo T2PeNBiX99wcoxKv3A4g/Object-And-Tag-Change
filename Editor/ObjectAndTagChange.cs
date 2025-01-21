94e89619d82dddf5674aec236a47042c51e7ba52
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-01-22 05:30:42 +0800

    Init

---
+++ Editor/ObjectAndTagChange.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+// From https://booth.pm/ja/items/6475280
+namespace __yky.ObjectAndTagChange.Editor
+{
+    public class SetObjectAndTag : EditorWindow
+    {
+        private static readonly Dictionary<int, string> OriginalTags = new();
+        private static readonly Dictionary<int, bool> WasActives = new();
+        private const string EditorOnlyTag = "EditorOnly";
+
+        [MenuItem("Tools/Set Object and Tag #e")]
+        [MenuItem("GameObject/yky/Set Object and Tag")]
+        private static void ToggleInactiveAndTag()
+        {
+            var selectedObjects = Selection.gameObjects;
+
+            if (selectedObjects.Length < 1)
+            {
+                Debug.LogWarning("オブジェクトが選択されていません。");
+                return;
+            }
+
+            foreach (var obj in selectedObjects)
+            {
+                var id = obj.GetInstanceID();
+                if (!OriginalTags.ContainsKey(id))
+                {
+                    OriginalTags.TryAdd(id, obj.tag);
+                    WasActives.TryAdd(id, obj.activeSelf);
+                }
+
+                if (obj.activeSelf)
+                {
+                    Undo.RecordObject(obj, "Change EditorOnly");
+                    obj.SetActive(false);
+                    obj.tag = EditorOnlyTag;
+                }
+                else
+                {
+                    var hasOriginalTag = OriginalTags.TryGetValue(id, out var originalTag);
+                    var hasActive = WasActives.TryGetValue(id, out var wasActive);
+
+                    Undo.RecordObject(obj, "Undo Tag");
+                    obj.SetActive(!hasActive || wasActive);
+                    if (obj.CompareTag(EditorOnlyTag))
+                        obj.tag = hasOriginalTag ? originalTag : "";
+
+                    OriginalTags.Remove(id);
+                    WasActives.Remove(id);
+                }
+
+                EditorUtility.SetDirty(obj);
+            }
+        }
+    }
+}
